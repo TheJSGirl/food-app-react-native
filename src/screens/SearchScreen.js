ccfc81d2e61c113877a340657575f71e30ef1ce6
Priyanka Negi <prinegi100@gmail.com>
2020-05-18 20:20:16 +0530

    feat: made api call

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -6,14 +6,35 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setSearchTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMsg, setErrorMsg] = useState('');
 
+    useEffect(() => {
+        searchApi('');
+    
+    }, [])
     const searchApi = async () => {
-        const response = await yelp.get('/search');
-        setResults(response.data.business);
+       try {
+        const response = await yelp.get('/search', {
+            params: {
+             limit: 50,
+             term,
+             location: 'san jose'
+            }
+         });
+         setResults(response.data.businesses);
+       }
+       catch(err) {
+           setErrorMsg('Something went wrong')
+       }
     }
     return <View>
-        <SearchBar term={term} onTermChange= {newTerm => setSearchTerm(newTerm)} />
-        <Text>Search Screen</Text>
+        <SearchBar 
+            term={term} 
+            onTermChange = {newTerm => setSearchTerm(newTerm)} 
+            onTermSubmit = {searchApi}
+        />
+        {/* {errorMsg ? <Text>{errorMsg}</Text> : null} */}
+        <Text>result {results.length}</Text>
     </View>
 }
 
